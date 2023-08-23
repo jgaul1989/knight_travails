8af08cd5750de2afcb7b697ba96b52362b955c45
Jonathan <jonathan.gaul1@gmail.com>
2023-08-22 20:00:52 -0400

    Fixed a big in valid move function
    Fixed a bug that did not count moves with a 0 coordinate
    as a valid move

--- knight.rb
+++ knight.rb
@@ -8,14 +8,16 @@ class Knight
   def initialize(coordinate, parent = nil)
     @parent = parent
     @coordinates = coordinate
-    @children = Array.new()
+    @children = []
   end
 
   def set_children(moves)
-    moves.each {|move| valid_move?(move) ? @children.push(Knight.new(move,self)) : @children.push(nil)}
+    # if a move is valid then set a new node else set it to nil
+    moves.each { |move| valid_move?(move) ? @children.push(Knight.new(move, self)) : @children.push(nil) }
   end
 
   def self.possible_moves(curr_location)
+    # return a list of all possible moves a knight can make
     moves = []
     moves.push([curr_location[0] + 2, curr_location[1] + 1])
     moves.push([curr_location[0] + 1, curr_location[1] + 2])
@@ -27,10 +29,12 @@ class Knight
     moves.push([curr_location[0] + 2, curr_location[1] - 1])
     moves
   end
+
   private
 
   def valid_move?(move)
-    move[0].positive? && move[0] < 8 && move[1].positive? && move[1] < 8
+    # check if a move is a valid location on the chess board
+    move[0] >= 0 && move[0] < 8 && move[1] >= 0 && move[1] < 8
   end
 
   def to_s
